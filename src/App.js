c45146a1704b5ab974234b685e20c22917ca6d8d
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-10-05 21:21:22 +0200

    :kissing_cat:

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
   const [activities, setActivities] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   //Strava Credentials
-  let clientId = "114612";
-  let clientSecret = "63304759e6167a310dcb69df8b78ace7af918ab7";
+  let clientId = process.env.REACT_APP_CLIENT_ID;
+  let clientSecret = process.env.REACT_APP_CLIENT_SECRET;
 
   //Refresh token and call address
-  const refreshToken =  "4e1d867906d392a9cf8a8f26d888c952c8796ee0";
+  const refreshToken =  process.env.REACT_APP_REFRESH_TOKEN;
   const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
 
   const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`
