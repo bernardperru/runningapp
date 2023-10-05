d7dab9fc5519a7b7f244fab7ce53f72dc3a34d9a
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-10-05 11:26:22 +0200

    123

---
+++ src/Activity.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Activity(props) {
+    return <div>this is an activity</div>;
+}
+
+export default Activity;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Activity from './Activity'
 
 function App() {
+  const [activities, setActivities] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  //Strava Credentials
+  let clientId = "114612";
+  let clientSecret = "63304759e6167a310dcb69df8b78ace7af918ab7";
+
+  //Refresh token and call address
+  const refreshToken =  "4e1d867906d392a9cf8a8f26d888c952c8796ee0";
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
+
+  const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`
+
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await fetch(callRefresh, {method: 'POST'});
+        const json = await data.json();
+        const data1 = await fetch(callActivities + json.access_token);
+        const json1 = await data1.json();
+        setActivities(json1);
+      } catch (error) {console.error("Error:", error)};
+    })();
+    
+  }, [callRefresh])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{activities.map((activity) => {
+      return <h1> {JSON.stringify(activity)} </h1>;
+    })}</div>
   );
 }
 
