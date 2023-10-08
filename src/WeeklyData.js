fb04d6d476db23b2a3e0219ceb2cb87fb1908ba0
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-10-08 11:54:39 +0200

    Update WeeklyData.js
    Added numbers for average mileage and goal mileage (avg + 10%)

--- src/WeeklyData.js
+++ src/WeeklyData.js
@@ -4,14 +4,26 @@ import { weekDistanceCumulative } from './funktioner';
 
 function WeeklyData(props) {
    const hash = weekDistanceCumulative(props.activities);
+   let averageMileage = 0;
+    Object.keys(hash).map((key) => {
+        averageMileage += hash[key];
+   })
 
+   averageMileage = ((averageMileage / Object.keys(hash).length) / 1000).toFixed(2); 
+   let goalMileage = (averageMileage / 10);
+   goalMileage += parseFloat(averageMileage);
     return (
-    <div> {Object.keys(hash).map((key) => 
-        {
-            return (<p>Week: {key} distance: {(hash[key] / 1000).toFixed(2)} km</p>)
-        }
-        )}
-    </div>)
+    <>
+        <div>{averageMileage}</div>
+        <div>{goalMileage}</div>
+        <div> {Object.keys(hash).map((key) => 
+            {
+                return (<p>Week: {key} distance: {(hash[key] / 1000).toFixed(2)} km</p>)
+            }
+            )}
+        </div>
+    </>
+    )
 }
 
 
