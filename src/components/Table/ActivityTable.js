ffbaf32ceeb23f876a8666876d6bf867a9663d02
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-10-14 20:33:31 +0200

    Update ActivityTable.js
    Fewer Lines, less functionality

--- src/components/Table/ActivityTable.js
+++ src/components/Table/ActivityTable.js
@@ -1,95 +1,103 @@
-import React from 'react';
-import { prettyDate, addZones, average } from '../../funktioner';
+import React from "react";
+import { prettyDate, addZones, average } from "../../funktioner";
 import "./ActivityTable.css";
 import { BsFillCaretUpFill, BsFillCaretDownFill } from "react-icons/bs";
 
-function ActivityTable({activities}) {
-    const [sort, setSort] = React.useState({keyToSort: "start_date", direction: "asc"});
+function ActivityTable({ activities }) {
+  const [sort, setSort] = React.useState({
+    keyToSort: "start_date",
+    direction: "asc",
+  });
+  //expand activities with zones
+  addZones(activities);
+  console.log(activities);
 
-    //expand activities with zones
-    addZones(activities)
-    console.log(activities)
+  const headers = [
+    {
+      id: 1,
+      key: "start_date",
+      label: "Date",
+    },
+    {
+      id: 2,
+      key: "distance",
+      label: "Distance",
+    },
+    {
+      id: 3,
+      key: "average_cadence",
+      label: "Avg. Cadence",
+    },
+    {
+      id: 4,
+      key: "elapsed_time",
+      label: "Time",
+    },
+    {
+      id: 5,
+      key: "average_heartrate",
+      label: "Avg. Heartrate",
+    },
+    {
+      id: 6,
+      key: "zone",
+      label: "Zone",
+    },
+  ];
 
-    const headers = [
-        {
-            id: 1,
-            key: "start_date",
-            label: "Date"
-        },
-        {
-            id: 2,
-            key: "distance",
-            label: "Distance"
-        },
-        {
-            id: 3,
-            key: "average_cadence",
-            label: "Avg. Cadence"
-        },
-        {
-            id: 4,
-            key: "elapsed_time",
-            label: "Time"
-        },
-        {
-            id: 5,
-            key: "average_heartrate",
-            label: "Avg. Heartrate"
-        },
-        {
-            id: 6,
-            key: "zone",
-            label: "Zone"
-        }
-    ]
+  function handleHeaderClick(header) {
+    setSort({
+      keyToSort: header.key,
+      direction:
+        header.key === sort.keyToSort
+          ? sort.direction === "asc"
+            ? "desc"
+            : "asc"
+          : "desc",
+    });
+  }
 
-    function handleHeaderClick(header) {
-        setSort(
-        {
-            keyToSort: header.key,
-            direction: 
-                header.key === sort.keyToSort ? sort.direction === 'asc' ? 'desc' : 'asc' : 'desc',        
-        });
+  function getSortedArray() {
+    if (sort.direction === "asc") {
+      return activities.sort((a, b) =>
+        a[sort.keyToSort] > b[sort.keyToSort] ? 1 : -1
+      );
     }
+    return activities.sort((a, b) =>
+      a[sort.keyToSort] > b[sort.keyToSort] ? -1 : 1
+    );
+  }
 
-    function getSortedArray () {
-        if (sort.direction === "asc") {
-            return activities.sort((a,b) => (a[sort.keyToSort] > b[sort.keyToSort] ? 1 : -1));
-        }
-        return activities.sort((a,b) => (a[sort.keyToSort] > b[sort.keyToSort] ? -1 : 1));
-    }
-
-    return (
-        <table className="center">
-            <thead>
-                <tr> 
-                    {headers.map((header, index) => (
-                        <th key={index} onClick={() => handleHeaderClick(header)}> 
-                            <div className="header-container">
-                                <span>{header.label}</span> 
-                                {
-                                    sort.keyToSort === header.key && (sort.direction === "asc" ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>)
-                                }
-                            </div>
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {getSortedArray().map(activity => (
-                <tr key={activity.id}>
-                    <td>{prettyDate(activity['start_date'])}</td>
-                    <td>{(activity.distance / 1000).toFixed(2)} km</td>
-                    <td>{(activity.average_cadence*2).toFixed(0)} spm</td>
-                    <td>{(activity.elapsed_time / 60).toFixed(2)} min</td>
-                    <td>{(activity.average_heartrate).toFixed(0)} bpm</td>
-                    <td>{activity.zone}</td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
+  return (
+    <table className="center">
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index} onClick={() => handleHeaderClick(header)}>
+              <div className="header-container">
+                <span>{header.label}</span>
+                {sort.keyToSort === header.key &&
+                  (sort.direction === "asc" ? (
+                    <BsFillCaretUpFill />
+                  ) : (
+                    <BsFillCaretDownFill />
+                  ))}
+              </div>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {activities.map((activity) => (
+          <tr>
+            {headers.map((header) => (
+              <td> {activity[header.key]} </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
-
 
 export default ActivityTable;
