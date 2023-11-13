087250df8baf542251ebe2f745da072de60b713f
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-11-13 18:41:19 +0100

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ function Home() {
 	const { REACT_APP_CLIENT_ID } = process.env;
 	const redirectUrl = 'http://localhost:3000/redirect';
 	const scope = 'activity:read_all';
+	const connectedToStrava = localStorage.getItem('hasRefreshToken');
 
 	const handleLogin = () => {
 		window.location.href = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=${scope}`;
@@ -16,11 +17,10 @@ function Home() {
 
 	return (
 		<div>
-			{localStorage.getItem('hasRefreshToken') === 'false' ? (
-				<button onClick={handleLogin}> you aren't connected to a strava account</button>
-			) : (
-				<div>You are connected to a strava account</div>
-			)}
+			{connectedToStrava === null ||
+				(connectedToStrava === 'false' && (
+					<button onClick={handleLogin}> you aren't connected to a strava account</button>
+				))}
 		</div>
 	);
 }
