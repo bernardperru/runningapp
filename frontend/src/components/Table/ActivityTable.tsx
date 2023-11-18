f71e692b27cef5a14357d668bd194c346058c147
Yann Perruchon <47976460+bernardperru@users.noreply.github.com>
2023-11-18 12:30:11 +0100

    Optimized sub-optimal code

--- frontend/src/components/Table/ActivityTable.tsx
+++ frontend/src/components/Table/ActivityTable.tsx
@@ -4,6 +4,8 @@ import { BsFillCaretUpFill, BsFillCaretDownFill } from 'react-icons/bs';
 import { GQLActivity, useGetActivitiesQuery } from '../../graphql';
 import { activityType } from '../../utils/constants';
 
+//this is my comment :)
+
 const labels: { [key in keyof activityType]: string } = {
 	start_date: 'Date',
 	distance: 'Distance',
