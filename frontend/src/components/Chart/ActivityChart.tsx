355ae5b89f19dcbf443d3ba472ed51b28521c685
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-12-01 14:59:04 +0100

    Revert "Revert "Added ChartPage to routing""
    This reverts commit 4f40587d7033cf45363215be80f1ec9896583684.

--- frontend/src/components/Chart/ActivityChart.tsx
+++ frontend/src/components/Chart/ActivityChart.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { format, formatDate } from '../../utils/utils';
+import { activityType } from '../../utils/constants';
+
 import {
 	Chart as ChartJS,
 	CategoryScale,
@@ -18,15 +21,17 @@ import { useGetActivitiesQuery, GQLActivity } from '../../graphql';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-const LineChart: React.FunctionComponent = () => {
+const ActivityChart: React.FunctionComponent = () => {
 	const { data } = useGetActivitiesQuery();
 	const [chartAxis, setChartAxis] = React.useState({
 		x: '',
 		y: '',
 	});
 
-	if (data !== undefined) {
+	if (data) {
 		const activities = [...data?.getActivities];
+
+		console.log(activities.length);
 
 		const options: ChartOptions<keyof ChartTypeRegistry> = {
 			responsive: true,
@@ -42,18 +47,22 @@ const LineChart: React.FunctionComponent = () => {
 			},
 		};
 
-		const labels = activities.map(activity => {
-			return activity.start_date;
-		});
+		const labels = activities
+			.filter(activity => activity.distance >= 5000 && activity.distance <= 6000)
+			.map(activity => {
+				return formatDate(activity.start_date);
+			});
 
 		const datax: ChartData<keyof ChartTypeRegistry> = {
 			labels,
 			datasets: [
 				{
 					label: 'distance',
-					data: activities.map(activity => {
-						return activity['distance'];
-					}),
+					data: activities
+						.filter(activity => activity.distance >= 5000 && activity.distance <= 6000)
+						.map(activity => {
+							return activity.average_heartrate;
+						}),
 					borderColor: 'rgb(53, 162, 235)',
 					backgroundColor: 'rgba(53, 162, 235, 0.5)',
 				},
@@ -64,4 +73,4 @@ const LineChart: React.FunctionComponent = () => {
 	}
 };
 
-export default LineChart;
+export default ActivityChart;
