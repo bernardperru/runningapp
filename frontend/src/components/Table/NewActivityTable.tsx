7629b2b38fb223b0e940f50ff4d83b96f4148bd9
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-12-16 17:43:15 +0100

    Update NewActivityTable.tsx

--- frontend/src/components/Table/NewActivityTable.tsx
+++ frontend/src/components/Table/NewActivityTable.tsx
@@ -16,27 +16,73 @@ const labels: { [key in keyof activityType]: string } = {
 };
 
 const NewActivityTable: React.FunctionComponent = () => {
-	const page = 0;
-	const offset = 15;
-	const { data, loading, error, networkStatus } = useGetAcivityPageQuery({
+	const [offset, setOffset] = React.useState(15);
+	const [page, setPage] = React.useState(0);
+	const [pages, setPages] = React.useState<{ pages: Array<number> }>();
+	const [sort, setSort] = React.useState<{
+		sort: keyof activityType;
+		order: 'asc' | 'desc';
+	}>({
+		sort: 'start_date',
+		order: 'asc',
+	});
+
+	const { data, loading, error, fetchMore } = useGetAcivityPageQuery({
 		variables: {
 			input: {
 				first: 15,
-				offset: page * offset,
-				order: 'desc',
-				sort: 'distance',
+				offset: offset,
+				order: sort.order,
+				sort: sort.sort,
 			},
 		},
 		notifyOnNetworkStatusChange: true,
 	});
 
-	if (data) {
-		console.log({ data });
+	React.useEffect(() => {
+		if (data) {
+			setPages({
+				pages: Array.from(Array(data.getActivityPage.count / offset).keys()).map(x => x + 1),
+			});
+		}
+	}, [data]);
+
+	//make useState to capture current page, sort, and order (asc / desc)
+
+	if (data && pages) {
 		return (
 			<div>
 				{' '}
 				<div>{data && data.getActivityPage.activities.map(activity => <div>{activity.distance}</div>)}</div>
 				<div>{data.getActivityPage.count}</div>
+				<div className="flex justify-center">
+					{page > 1 && (
+						<button
+							onClick={() => setPage(page - 1)}
+							className="h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white rounded-l-lg focus:shadow-outline hover:bg-indigo-100">
+							Previous
+						</button>
+					)}
+					{page === 1 && <button className="h-10 px-5 text-indigo-100  bg-white rounded-l-lg">Previous</button>}
+					{pages.pages.map((el, index) => (
+						<button
+							key={index}
+							onClick={() => setPage(el)}
+							className="h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white rounded-l-lg focus:shadow-outline hover:bg-indigo-100">
+							{el}
+						</button>
+					))}
+					{page < data.getActivityPage.count / offset && (
+						<button
+							onClick={() => setPage(page + 1)}
+							className="h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white rounded-l-lg focus:shadow-outline hover:bg-indigo-100">
+							Next
+						</button>
+					)}
+					{page === data.getActivityPage.count && (
+						<button className="h-10 px-5 text-indigo-100 bg-white rounded-l-lg">Next</button>
+					)}
+				</div>{' '}
 			</div>
 		);
 	}
