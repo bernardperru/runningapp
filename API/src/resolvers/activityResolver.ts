a7ee084151b7a6cca35c8bb164d770dd9ea619ea
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-01-05 16:58:01 +0100

    Added query to fetch new activities from strava
    Returns the amount of *new* activities added to the database

--- API/src/resolvers/activityResolver.ts
+++ API/src/resolvers/activityResolver.ts
@@ -100,5 +100,78 @@ export const activityResolver: GQLResolvers = {
         currentPage,
       };
     },
+    updateActivities: async (_, args, context) => {
+      if (!context.auth) {
+        throw new Error("User not logged in");
+      }
+
+      if (!context.auth.user.refresh_token) {
+        throw new Error("No strava refresh token assigned to User");
+      }
+
+      const activities = await context.auth.stravaAPI.getListActivities(
+        context.auth.user.refresh_token
+      );
+
+      if (!activities) {
+        throw new Error("activities not returned from Strava");
+      }
+
+      // const data = activities.map((activity) => {
+      //   return {
+      //     userId: context.auth ? context.auth.user.id : 0,
+      //     activityId: activity.activityId,
+      //     distance: activity.distance,
+      //     elapsed_time: activity.elapsed_time,
+      //     start_date: activity.start_date,
+      //     summary_polyline: activity.summary_polyline,
+      //     average_cadence: activity.average_cadence,
+      //     average_heartrate: activity.average_heartrate,
+      //     average_pace: activity.average_pace,
+      //     zone: activity.zone,
+      //     weekId: undefined,
+      //   } satisfies Prisma.ActivityCreateManyInput;
+      // });
+
+      const oldActivityCount = await database.activity.count({
+        where: {
+          userId: context.auth.user.id,
+        },
+      });
+
+      const upsertActivities = await Promise.all(
+        activities.map(async (activity) => {
+          await database.activity.upsert({
+            where: { activityId: activity.activityId },
+            update: {},
+            create: {
+              user: {
+                connect: {
+                  id: context.auth ? context.auth.user.id : 0,
+                },
+              },
+              activityId: activity.activityId,
+              distance: activity.distance,
+              elapsed_time: activity.elapsed_time,
+              start_date: activity.start_date,
+              summary_polyline: activity.summary_polyline,
+              average_cadence: activity.average_cadence,
+              average_heartrate: activity.average_heartrate,
+              average_pace: activity.average_pace,
+              zone: activity.zone,
+              weekId: undefined,
+            },
+          });
+        })
+      );
+
+      const newActivityCount = await database.activity.count({
+        where: {
+          userId: context.auth.user.id,
+        },
+      });
+
+      return newActivityCount - oldActivityCount;
+    },
   },
 };
