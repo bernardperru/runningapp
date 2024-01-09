799bacaf0a5d6f5fad522309d9219a2505f6f3ef
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-01-09 12:42:12 +0100

    Update Card.tsx

--- frontend/src/components/Cards/Card.tsx
+++ frontend/src/components/Cards/Card.tsx
@@ -17,7 +17,7 @@ export function Card<T>({ data, fields, title, interact }: Props<T>) {
 	return (
 		<div className="bg-grey-300 hover:bg-sky-300 shadow-lg rounded-md py-7 px-12 mt-6" onClick={() => interact(data)}>
 			<h1 className="text-2xl text-gray-900 font-normal border-b-2 border-black w-fit">
-				{title ? title.map(a => get(data, a) + ' ') : ''}
+				{title.map(a => get(data, a) + ' ')}
 			</h1>
 			<ul className="">
 				{fields.map((field, fieldIndex) => (
