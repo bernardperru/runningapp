3d2f07c1a003bc24c8c40cf5f9daa2a5a26cdd55
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-01-16 00:42:32 +0100

    Update RunMap.tsx
    Defined an interface for the data to be shown inside the runmap

--- frontend/src/components/Map/RunMap.tsx
+++ frontend/src/components/Map/RunMap.tsx
@@ -1,53 +1,42 @@
 import React from 'react';
-import { format } from '../../utils/utils';
+import { formatDate } from '../../utils/utils';
 import { MapContainer, TileLayer, Popup, Polyline } from 'react-leaflet';
 import './RunMap.css';
 import polyline from '@mapbox/polyline';
 import { GQLActivity } from '../../graphql';
-import { activityType } from '../../utils/constants';
 
-type label = {
-	label: string;
-	type: 'avg' | 'sum' | 'none';
-};
+interface IMapData {
+	key: keyof GQLActivity;
+	render: (value: GQLActivity) => JSX.Element;
+}
 
-const mapStats: { [key in keyof activityType]: label } = {
-	distance: { label: 'Distance', type: 'sum' },
-	elapsed_time: { label: 'Time', type: 'sum' },
-	average_heartrate: { label: 'Average Heartrate', type: 'avg' },
-	average_cadence: { label: 'Average Cadence', type: 'avg' },
-	start_date: { label: 'Date', type: 'none' },
-	zone: { label: 'Zone', type: 'none' },
-	average_pace: { label: 'Average Pace', type: 'avg' },
-};
+const mapData: IMapData[] = [
+	{
+		key: 'start_date',
+		render: ({ start_date }) => {
+			return <div>Date: {formatDate(start_date)}</div>;
+		},
+	},
+	{
+		key: 'distance',
+		render: ({ distance }) => {
+			return <div>Distance: {(distance / 1000).toFixed(2) + ' km'}</div>;
+		},
+	},
+];
 
 const RunMap: React.FunctionComponent<{ activity: GQLActivity }> = ({ activity }) => {
-	if (!activity) {
-		return <div></div>;
-	}
-
 	const pline = polyline.decode(activity.summary_polyline);
-
-	const keys = (Object.keys(activity) as (keyof activityType)[]).filter(key => {
-		return mapStats[key];
-	});
 
 	return (
 		<div id="map">
-			<MapContainer center={pline[0]} zoom={13} scrollWheelZoom={false}>
+			<MapContainer center={pline[0]} zoom={13} scrollWheelZoom={true}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
 				<Polyline positions={pline}>
-					<Popup>
-						{keys.map(
-							key =>
-								mapStats[key].type !== 'none' && (
-									<div key={key}>{mapStats[key].label + ' : ' + format(key, activity[key])}</div>
-								)
-						)}
-					</Popup>
+					<Popup>{mapData.map(dataField => dataField.render(activity))}</Popup>
 				</Polyline>
 			</MapContainer>
 		</div>
