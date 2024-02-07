128b12ca960b45ea7a5eb9a612a1679be4d829d7
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-02-07 11:10:37 +0100

    strict mode removed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:4000/',
+	uri: 'https://runningapp-service.onrender.com',
 });
 
 const authLink = setContext((_, { headers }) => {
