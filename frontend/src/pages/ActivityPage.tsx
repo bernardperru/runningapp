6823f1b74d5d4ca37afb4166f48ac71a645220d8
bernardperru <47976460+bernardperru@users.noreply.github.com>
2024-03-01 15:55:47 +0100

    Update ActivityPage.tsx

--- frontend/src/pages/ActivityPage.tsx
+++ frontend/src/pages/ActivityPage.tsx
@@ -2,9 +2,7 @@ import { useLocation } from 'react-router-dom';
 import { useOutletContext } from 'react-router-dom';
 
 export function ActivityPage() {
-	const activityId = useOutletContext();
 	const location = useLocation();
-	console.log(location.state);
 
 	if (location.state) {
 		return <div></div>;
